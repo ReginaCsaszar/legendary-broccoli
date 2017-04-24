cc5c385d77dc2fbf67adb5581421797a248224db
ReginaCsaszar <regina.csaszar@gmail.com>
2017-04-24 15:03:34 +0200

    projects submenus done

---
+++ projects.py
@@ -0,0 +1,43 @@
+"""Program parts for projects"""
+
+import menu
+
+
+def archived_projects():
+    """Archived projects handler menu"""
+    title = "Archived projects"
+    menu_items = ("List all archived projects", "Search in archives", "Show project details", "Show project statistics")
+    cont = True
+    while cont:
+        option = menu_drawer(title, menu_items, "Back to projects")
+        if option == 1:
+            continue
+        elif option == 2:
+            continue
+        elif option == 3:
+            continue
+        elif option == 4:
+            continue
+        else:
+            cont = False
+    return False
+
+
+def active_projects():
+    """Archive projects handler menu"""
+    title = "Active projects"
+    menu_items = ("Add new project", "Delete project", "Show project details", "Show project statistic")
+    cont = True
+    while cont:
+        option = menu_drawer(title, menu_items, "Back to projects")
+        if option == 1:
+            continue
+        elif option == 2:
+            continue
+        elif option == 3:
+            continue
+        elif option == 4:
+            continue
+        else:
+            cont = False
+    return False
