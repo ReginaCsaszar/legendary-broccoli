171095173555c2e5497a46d70eab26b93dcbe0c8
ReginaCsaszar <regina.csaszar@gmail.com>
2017-08-06 19:07:59 +0200

    created separated logic for users/teams and tasks/projects

---
+++ tasks.py
@@ -0,0 +1,9 @@
+"""This module handles the task-, and project-related events.
+It connects to the data_handler module if necessary and
+handles the returned results.
+Pass the processed data back to the main routing module.
+
+"""
+
+
+import data_handler
---
+++ users.py
@@ -0,0 +1,8 @@
+"""This module handles the user-, and team-related events.
+It connects to the data_handler module if necessary and
+handles the returned results.
+Pass the processed data back to the main routing module.
+
+"""
+
+import data_handler
